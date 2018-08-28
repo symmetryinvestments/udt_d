6e31c7ca6f8fdbdb9f3844e068f9aac7680a5137
Laeeth Isharc <laeeth@kaleidic.io>
2018-08-28 01:12:27 +0100

    appclient and server now build

--- source/build.sh
+++ source/build.sh
@@ -1,2 +1,5 @@
-d++ -c udtwrap.dpp appclient.dpp
-g++ udtwrap.o libudtc.a libudt.a -L-lstdc++ -lpthread -lphobos2 -L-L~/dlang/dmd-2.080.0/linux/lib64
+d++ -c --keep-d-files udtwrap.dpp
+d++ -c appclient.dpp
+d++ -c appserver.dpp
+g++ -o appclient appclient.o udtwrap.o libudtc.a libudt.a -L-lstdc++ -lpthread -lphobos2 -L-L~/dlang/dmd-2.080.0/linux/lib64
+g++ -o appserver appserver.o udtwrap.o libudtc.a libudt.a -L-lstdc++ -lpthread -lphobos2 -L-L~/dlang/dmd-2.080.0/linux/lib64
